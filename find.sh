b13e81da4ad5829e85d35ed3d0bcdffebcc9b70a
E.K.Jithendiran <51771468+Jithendiran@users.noreply.github.com>
2021-04-08 11:52:59 +0530

    all done

--- find.sh
+++ find.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#find -type f \( -name *.jpg -o -name *.pdf \)
 
-#find /home/jidesh/ML/Neural\ Network/dogs-vs-cats/train/ -iname '*.jpg' -exec ln -sf -t /home/jidesh/GALLERY/ {} +;
 
 name=()
 File_Formats=$(<files.txt)
@@ -32,10 +30,5 @@ do
 	find ~ -path $dir -prune -o  -iname $i -exec ln -sf -t $dir/ {} +;
 done
 
-#sudo chmod -R 774 $dir 
-#echo ${#path[@]}
-#echo $(basename ${path[2]})
-#echo $(dirname ${path[2]})
-#echo ${path[2]}
-#ln -sf "${path[2,0]}" /home/jidesh/
-successbeep
+
+
